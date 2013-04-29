b19bd893e0b0f5b0fdc8f6c0198343037f65f295
Stanislav Termosa <termosa.stanislav@gmail.com>
2013-04-29 09:46:32 +0300

    Change structure

--- www/public/index.php
+++ www/public/index.php
@@ -1,6 +1,6 @@
 <?php
-require 'src/lib/Debug/Debugger.php';
-require 'src/App.php';
+require '../src/lib/Debug/Debugger.php';
+require '../src/App.php';
 inc('HTML_Helpers');
 run($_query); // Запускаем приложение
 // TODO: для маршрутов сделать возможность указывать родительскую папку (..)
